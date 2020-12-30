109b8c94ac7cfe6a110fb1c6e6570ec9af619411
Chinyerem Emmanuel <kelvinesthy@gmail.com>
2020-12-30 10:45:42 +0100

    second changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
